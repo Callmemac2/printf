de69a76722425cec4001da440f669d63abfb9a65
Serge <98883394+Kasberg0@users.noreply.github.com>
2022-10-16 23:03:56 +0000

    Task 0

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
+#inclide <unistd.h>
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
@@ -93,7 +93,7 @@ int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* width handler */
-int handle_write_char(char c, char buffer[],
+int handle_wrute_char(char c, char buffer[],
 	int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
 	int flags, int width, int precision, int size);
